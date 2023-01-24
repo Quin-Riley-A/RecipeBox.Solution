f8e1d685b69f2225824b8e8ee5e50f35dd57a3c7
pphila <efl2ice89@gmail.com>
2023-01-23 20:57:54 -0800

    add range and error message to ratings in recipe model, updated migration, added rating to create view
    Co-authored-by: Hans Ellis <bullhorn@gmail.com>
    Co-authored-by: Quin Asselin <quin.asselin1@gmail.com>

--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -10,6 +10,8 @@ namespace RecipeBox.Models
     public string Name { get; set; }
     public string Ingredients { get; set; }
     public string Instruction { get; set; }
+    [Range(1, 5, ErrorMessage = "Enter A Rating of 1-5")]
+    public int Rating { get; set; }
     public List<RecipeTag> JoinEntities { get; }
     public ApplicationUser User { get; set; }
   }
